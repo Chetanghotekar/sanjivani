282f09bba9f4828bca88ff10e966d3a1c4e6f14b
Chetanghotekar <chetan.ghotekar_24uds@sanjivani.edu.in>
2024-12-27 23:10:19 +0530

    Update conEx1.java

--- conEx1.java
+++ conEx1.java
@@ -6,12 +6,11 @@ class conEx1
      int i; 
      for(i=1;i<=10;i++)
      { 
-         //System.out.println(i);
-         if(i==5)
+         System.out.println(i);
+         if(i==3)
           { 
            continue;
           }
-        System.out.println(i);
    } 
  }
 }
